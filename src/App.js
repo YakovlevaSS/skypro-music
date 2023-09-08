2fd0f38f33d88a0365bf7af48a517bcbac0c576b
YakovlevaSS <sofyakovleva95@gmail.com>
2023-09-08 22:37:41 +0300

    Добавлены компоненты Search, Filter

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@
 import './App.css'
 import { PlayList } from './components/PlayList/PlayList'
 import { Nav } from './components/Nav/Nav'
+import { Search } from './components/Search/Search'
+import { Filter } from './components/Filter/Filter'
 
 function App() {
   return (
@@ -14,30 +16,9 @@ function App() {
           <main className="main">
             <Nav/>
             <div className="main__centerblock centerblock">
-              <div className="centerblock__search search">
-                <svg className="search__svg">
-                  <use xlinkHref="img/icon/sprite.svg#icon-search" />
-                </svg>
-                <input
-                  className="search__text"
-                  type="search"
-                  placeholder="Поиск"
-                  name="search"
-                />
-              </div>
+            <Search/>
               <h2 className="centerblock__h2">Треки</h2>
-              <div className="centerblock__filter filter">
-                <div className="filter__title">Искать по:</div>
-                <div className="filter__button button-author _btn-text">
-                  исполнителю
-                </div>
-                <div className="filter__button button-year _btn-text">
-                  году выпуска
-                </div>
-                <div className="filter__button button-genre _btn-text">
-                  жанру
-                </div>
-              </div>
+              <Filter/>
               <div className="centerblock__content">
                 <div className="content__title playlist-title">
                   <div className="playlist-title__col col01">Трек</div>
