cede86814ce0f085ea2e003439c14f310833d470
YakovlevaSS <sofyakovleva95@gmail.com>
2023-09-13 16:11:31 +0300

    созданы массимы для рендеринга выпадающих списков

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
-// import { trackArr } from "../../utilits/trackArr";
+import { trackArr } from '../../utilits/trackArr'
 import './filter.css'
 /* eslint-disable import/prefer-default-export */
 
 export function Filter() {
-
   // создаем переменные состояния для каждой кнопки
   const [visibleAuthor, setVisibleAuthor] = useState(false)
   const [visibleYear, setVisibleYear] = useState(false)
@@ -23,22 +22,47 @@ export function Filter() {
     setVisibleYear(!visibleYear)
   }
 
-  const toggleVisibilityGenre= () => {
+  const toggleVisibilityGenre = () => {
     setVisibleAuthor(false)
     setVisibleGenre(!visibleGenre)
     setVisibleYear(false)
   }
 
   // создание массивов из годов, авторов и жанров из существующих массивов треков
-
-
+  const authorArr = Array.from(
+    new Set(trackArr.map((track) => track.trackAuthorLink)),
+  )
+  const yearArr = Array.from(
+    new Set(trackArr.map((track) => track.year))
+    )
+  const genrerArr = Array.from(
+    new Set(trackArr.map((track) => track.genre))
+    )
 
   return (
     <div className="centerblock__filter filter">
       <div className="filter__title">Искать по:</div>
-      <button className="filter__button button-author _btn-text" type="button" onClick={toggleVisibilityAuthor}>исполнителю</button>
-      <button className="filter__button button-year _btn-text" type="button" onClick={toggleVisibilityYear}>году выпуска</button>
-      <button  className="filter__button button-genre _btn-text" type="button" onClick={toggleVisibilityGenre}>жанру</button>
+      <button
+        className="filter__button button-author _btn-text"
+        type="button"
+        onClick={toggleVisibilityAuthor}
+      >
+        исполнителю
+      </button>
+      <button
+        className="filter__button button-year _btn-text"
+        type="button"
+        onClick={toggleVisibilityYear}
+      >
+        году выпуска
+      </button>
+      <button
+        className="filter__button button-genre _btn-text"
+        type="button"
+        onClick={toggleVisibilityGenre}
+      >
+        жанру
+      </button>
     </div>
   )
 }
