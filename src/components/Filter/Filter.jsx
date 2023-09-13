a715a293fb9b6948fd375100eca622a7cc579b99
YakovlevaSS <sofyakovleva95@gmail.com>
2023-09-13 11:59:34 +0300

    Настроены состояния для кнопок с фильтрами

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,12 +1,44 @@
+import { useState } from 'react'
+// import { trackArr } from "../../utilits/trackArr";
 import './filter.css'
 /* eslint-disable import/prefer-default-export */
+
 export function Filter() {
+
+  // создаем переменные состояния для каждой кнопки
+  const [visibleAuthor, setVisibleAuthor] = useState(false)
+  const [visibleYear, setVisibleYear] = useState(false)
+  const [visibleGenre, setVisibleGenre] = useState(false)
+
+  // функции изменения состояния при клике
+  const toggleVisibilityAuthor = () => {
+    setVisibleAuthor(!visibleAuthor)
+    setVisibleGenre(false)
+    setVisibleYear(false)
+  }
+
+  const toggleVisibilityYear = () => {
+    setVisibleAuthor(false)
+    setVisibleGenre(false)
+    setVisibleYear(!visibleYear)
+  }
+
+  const toggleVisibilityGenre= () => {
+    setVisibleAuthor(false)
+    setVisibleGenre(!visibleGenre)
+    setVisibleYear(false)
+  }
+
+  // создание массивов из годов, авторов и жанров из существующих массивов треков
+
+
+
   return (
     <div className="centerblock__filter filter">
       <div className="filter__title">Искать по:</div>
-      <div className="filter__button button-author _btn-text">исполнителю</div>
-      <div className="filter__button button-year _btn-text">году выпуска</div>
-      <div className="filter__button button-genre _btn-text">жанру</div>
+      <button className="filter__button button-author _btn-text" type="button" onClick={toggleVisibilityAuthor}>исполнителю</button>
+      <button className="filter__button button-year _btn-text" type="button" onClick={toggleVisibilityYear}>году выпуска</button>
+      <button  className="filter__button button-genre _btn-text" type="button" onClick={toggleVisibilityGenre}>жанру</button>
     </div>
   )
 }
