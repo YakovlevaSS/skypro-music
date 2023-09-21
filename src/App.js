795828ef6898eadae604899c95af8a03ff196df8
YakovlevaSS <sofyakovleva95@gmail.com>
2023-09-21 13:14:17 +0300

    GlobalStyle

--- src/App.js
+++ src/App.js
@@ -4,40 +4,44 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 // import React from 'react';
 import { useState, useEffect } from 'react'
-import './App.css'
+// import './App.css'
 import { Bar } from './components/Bar/Bar'
 import { CenterBlock } from './components/CenterBlock/CenterBlock'
-import Nav  from './components/Nav/Nav'
+import Nav from './components/Nav/Nav'
 import { SideBar } from './components/SideBar/SidBar'
 import { Footer } from './components/Footer/Footer'
+import { GlobalStyle } from './styles/global'
 import * as S from './styles/styles'
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false)
 
   useEffect(() => {
     if (!isLoaded) {
       const timeout = setTimeout(() => {
-        setIsLoaded(true);
-      }, 10000);
+        setIsLoaded(true)
+      }, 10000)
 
-      return () => clearTimeout(timeout); 
+      return () => clearTimeout(timeout)
     }
-  }, [isLoaded]);
+  }, [isLoaded])
   return (
-    <S.App>
-      <S.Wrapper>
-        <S.Container>
-          <S.Main>
-            <Nav/>
-            <CenterBlock isLoaded={isLoaded}/>
-            <SideBar isLoaded={isLoaded}/>
-          </S.Main>
-          <Bar isLoaded={isLoaded}/>
-          <Footer/>
-        </S.Container>
-      </S.Wrapper>
-    </S.App>
+    <>
+      <GlobalStyle />
+      <S.App>
+        <S.Wrapper>
+          <S.Container>
+            <S.Main>
+              <Nav />
+              <CenterBlock isLoaded={isLoaded} />
+              <SideBar isLoaded={isLoaded} />
+            </S.Main>
+            <Bar isLoaded={isLoaded} />
+            <Footer />
+          </S.Container>
+        </S.Wrapper>
+      </S.App>
+    </>
   )
 }
 
