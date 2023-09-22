44b3d8d743d66845b22cb6fb4afee63da6d63a58
YakovlevaSS <sofyakovleva95@gmail.com>
2023-09-22 11:50:49 +0300

    донастроены globalstyle

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,6 +2,19 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
 
 * {
   margin: 0;
@@ -41,4 +54,6 @@ ul li {
     width: 100%;
     height: 100%;
     font-family: StratosSkyeng, sans-serif;
-  }`
+  }
+
+  `
