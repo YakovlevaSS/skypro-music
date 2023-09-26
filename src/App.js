303a0a4708ecdb286966ca53cb2b23e25a45c6c7
YakovlevaSS <sofyakovleva95@gmail.com>
2023-09-26 13:01:15 +0300

    main page

--- src/App.js
+++ src/App.js
@@ -1,43 +1,15 @@
 /* eslint-disable consistent-return */
 // import React from 'react';
-import { useState, useEffect } from 'react'
 // import './App.css'
-import { Bar } from './components/Bar/Bar'
-import { CenterBlock } from './components/CenterBlock/CenterBlock'
-import Nav from './components/Nav/Nav'
-import { SideBar } from './components/SideBar/SidBar'
-import { Footer } from './components/Footer/Footer'
-import * as S from './styles/styles'
 import { GlobalStyle } from './global'
+import { AppRoutes } from "./routes";
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false)
 
-  useEffect(() => {
-    if (!isLoaded) {
-      const timeout = setTimeout(() => {
-        setIsLoaded(true)
-      }, 10000)
-
-      return () => clearTimeout(timeout)
-    }
-  }, [isLoaded])
   return (
     <>
       <GlobalStyle />
-      <S.App>
-        <S.Wrapper>
-          <S.Container>
-            <S.Main>
-              <Nav />
-              <CenterBlock isLoaded={isLoaded} />
-              <SideBar isLoaded={isLoaded} />
-            </S.Main>
-            <Bar isLoaded={isLoaded} />
-            <Footer />
-          </S.Container>
-        </S.Wrapper>
-      </S.App>
+      <AppRoutes />
     </>
   )
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,1 +1,10 @@
 import { Routes, Route } from "react-router-dom";
+import Main from "./pages/main";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
+  );
+};
