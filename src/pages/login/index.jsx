f61980a5602ec8db78cbff3a3136bb63a12ba24b
YakovlevaSS <sofyakovleva95@gmail.com>
2023-09-26 21:38:59 +0300

    added registration and login pages

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable consistent-return */
+import * as S from './styles'
+
+function LogIn() {
+<S.Wrapper>
+  <S.Container>
+    <S.ModalBlock>
+      <S.ModalFormLogin>
+        <S.ModalFormLoginLink  href="../">
+          <S.ModalLogo>
+            <S.ModalLogoImg src="../img/logo_modal.png" alt="logo" />
+          </S.ModalLogo>
+        </S.ModalFormLoginLink>
+        <S.ModalInput
+          type="text"
+          name="login"
+          placeholder="Почта"
+        />
+        <S.ModalInput
+          type="password"
+          name="password"
+          placeholder="Пароль"
+        />
+        <S.ModalButtonEnter>
+          <S.ModalButtonEnterLink href="../index.html">Войти</S.ModalButtonEnterLink>
+        </S.ModalButtonEnter>
+        <S.ModalButtonReg>
+          <S.ModalButtonRegLink href="../index.html">Зарегистрироваться</S.ModalButtonRegLink>
+        </S.ModalButtonReg>
+      </S.ModalFormLogin>
+    </S.ModalBlock>
+  </S.Container>
+</S.Wrapper>
+}
+
+export default LogIn
---
+++ src/pages/registration/index.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable consistent-return */
+import * as S from './styles'
+
+function Registration() {
+<S.Wrapper>
+  <S.Container>
+    <S.ModalBlock>
+      <S.ModalFormLogin>
+        <S.ModalFormLoginLink  href="../">
+          <S.ModalLogo>
+            <S.ModalLogoImg src="../img/logo_modal.png" alt="logo" />
+          </S.ModalLogo>
+        </S.ModalFormLoginLink>
+        <S.ModalInput
+          type="text"
+          name="login"
+          placeholder="Почта"
+        />
+        <S.ModalInput
+          type="password"
+          name="password"
+          placeholder="Пароль"
+        />
+        <S.ModalInput
+          type="password"
+          name="password"
+          placeholder="Повторите пароль"
+        />
+        <S.ModalButtonEnter>
+          <S.ModalButtonEnterLink href="../index.html">Зарегистрироваться</S.ModalButtonEnterLink>
+        </S.ModalButtonEnter>
+      </S.ModalFormLogin>
+    </S.ModalBlock>
+  </S.Container>
+</S.Wrapper>
+}
+
+export default Registration
