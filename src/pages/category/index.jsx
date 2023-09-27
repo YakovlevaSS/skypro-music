70abc381c83b02d9839827922200ccf15df715c4
YakovlevaSS <sofyakovleva95@gmail.com>
2023-09-27 12:17:23 +0300

    Added favorites and category pages

---
+++ src/pages/category/index.jsx
@@ -0,0 +1,10 @@
+import * as S from './styles';
+
+export function Category() {
+
+  return (
+    <S.CategoryBlock>
+      <h1>CategoryPage </h1>
+    </S.CategoryBlock>
+  );
+}
---
+++ src/pages/favorites/index.jsx
@@ -0,0 +1,10 @@
+import * as S from './styles';
+
+export function Favorites() {
+
+  return (
+    <S.FavoritesBlock>
+      <h1>Favorites tracks</h1>
+    </S.FavoritesBlock>
+  );
+}
