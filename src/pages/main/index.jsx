93f46090eb456fe859c09551c29eab862161ba74
YakovlevaSS <sofyakovleva95@gmail.com>
2023-10-01 14:46:29 +0300

    Get tracks from api

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -6,7 +6,6 @@ import Nav from '../../components/Nav/Nav'
 import SideBar from '../../components/SideBar/SidBar'
 import Footer from '../../components/Footer/Footer'
 import * as S from './styles'
-import { getAllTracks } from '../../api'
 
 function Main() {
   const [isLoaded, setIsLoaded] = useState(false)
@@ -20,8 +19,6 @@ function Main() {
       return () => clearTimeout(timeout)
     }
   }, [isLoaded])
-
-getAllTracks().then((tracks) => console.log(tracks))
   return (
         <S.Wrapper>
           <S.Container>
