ba0823baa020819c61538eaaf7866c7eece34e59
YakovlevaSS <sofyakovleva95@gmail.com>
2023-10-02 11:58:26 +0300

    cor. export

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 /* eslint-disable consistent-return */
 import { useState } from "react";
-import { GlobalStyle } from './global'
+import GlobalStyle from './global'
 import AppRoutes from './routes'
 
 
 function App() {
   const [user, setUser] = useState(false)
-  console.log(localStorage)
 
   const handleLogin = () => {
     localStorage.setItem('user', 'true');
     const curentLocalStorage = localStorage.getItem('user');
-    console.log(curentLocalStorage)
     setUser(curentLocalStorage);
   // setUser(true);
     // console.log(user)
