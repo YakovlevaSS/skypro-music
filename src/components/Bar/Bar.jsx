7252f27347a93b9a00aae105a116d94ed86449d1
YakovlevaSS <sofyakovleva95@gmail.com>
2023-10-07 14:16:08 +0300

    progressBar synchronization

--- src/components/Bar/Bar.jsx
+++ src/components/Bar/Bar.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import * as S from './styles'
 import Player from '../Player/Player'
 import VolumeBlock from '../VolumeBlock/VolumeBlock'
@@ -16,21 +16,43 @@ function Bar({
   const playRef = useRef(null)
   const [volume, setVolume] = useState(0.5)
 
-  const [currentTime, setCurrentTime] = useState(70);
-  const duration = 230;
+  const [currentTime, setCurrentTime] = useState(0)
+  const [duration, setDuration] = useState(0)
+
+  const handleTimeChange = (event) => {
+    setCurrentTime(event.target.value)
+  }
+
+  useEffect(() => {
+    const doNewTime = () => {
+      if (playRef.current) {
+        setCurrentTime(playRef.current.currentTime)
+        setDuration(playRef.current.duration)
+      } else {
+        setCurrentTime(0)
+        setDuration(0)
+      }
+    }
+
+    playRef.current.addEventListener('timeupdate', doNewTime)
+
+    return () => {
+      playRef.current.removeEventListener('timeupdate', doNewTime)
+    }
+  })
 
   return (
     <S.Bar>
       <S.BarContent>
-      <S.StyledProgressInput
-      type="range"
-      min={0}
-      max={duration}
-      value={currentTime}
-      step={0.01}
-      onChange={(event) => setCurrentTime(event.target.value)}
-      // $color="#ff0000"
-    />
+        <S.StyledProgressInput
+          type="range"
+          min={0}
+          max={duration}
+          value={currentTime}
+          step={0.01}
+          onChange={handleTimeChange}
+          // $color="#ff0000"
+        />
         <S.BarPlayerProgress />
         <S.BarPlayerBlock>
           <Player
@@ -45,10 +67,11 @@ function Bar({
             playRef={playRef}
             volume={volume}
           />
-          <VolumeBlock 
-          volume={volume}
-          setVolume={setVolume}
-          playRef={playRef} />
+          <VolumeBlock
+            volume={volume}
+            setVolume={setVolume}
+            playRef={playRef}
+          />
         </S.BarPlayerBlock>
       </S.BarContent>
     </S.Bar>
